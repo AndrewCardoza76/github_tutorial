f74045a947903031b6f02c8dcc043c3ae970fe67
AndrewCardoza76 <andrewcardoza76@g.ucla.edu>
2025-06-28 15:18:28 -0700

    Delete branch_activity.py

--- branch_activity.py
+++
@@ -1,9 +0,0 @@
-def median(input):
-  middleIndex = len(input)//2
-  if(len(input)%2==0):
-    medianValue = (input[middleIndex]+input[middleIndex-1])/2
-  else:
-    medianValue = input[middleIndex]
-  return medianValue
-my_list=[0,1,2,3,4,5]
-print(median(my_list))
